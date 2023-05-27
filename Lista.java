16d3297faa9f121e90658c99a891f9137d6d69a2
Heloíse Cardoso <104039569+HeloiseCardoso@users.noreply.github.com>
2023-05-27 12:33:59 -0300

    Update Lista.java

--- Lista.java
+++ Lista.java
@@ -91,5 +91,152 @@ public class Lista {
 
 }
 
+public class Contato {
+    private String nome;
+    private String endereco;
+    private long cpf;
 
+    public long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Contato(String nome, String endereco, long cpf) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.cpf = cpf;
+    }
+
+
+    public String getContato() {
+        String auxiliar = nome + " \n";
+        auxiliar = nome + endereco + "\n";
+        auxiliar = nome + endereco + Long.toString(cpf) + " \n";
+        return auxiliar;
+    }
+}
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+    static Contato criarContato() {
+
+        System.out.println("Digite o nome:");
+        scanner.nextLine();
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o endere�o:");
+        String endereco = scanner.nextLine();
+
+        System.out.println("Digite o cpf:");
+        long cpf = scanner.nextLong();
+
+        Contato contato = new Contato(nome, endereco, cpf);
+        return contato;
+    }
+
+    public static void main(String[] args) {
+        int opcao = 0, tamanho = 2;
+        Contato contato = null;
+
+        Lista lista = new Lista();
+
+        do {
+            System.out.println("Digite:");
+            System.out.println("1 - Para criar uma lista");
+            System.out.println("2 - Para inserir um contato no final da lista");
+            System.out.println("3 - Para pesquisar um contato na lista");
+            System.out.println("4 - Para retirar um contato da lista");
+            System.out.println("5 - Para mostrar a lista de contatos inteira");
+            System.out.println("6 - Para encerrar");
+            opcao = scanner.nextInt();
+            System.out.println();
+
+            switch(opcao) {
+                case 1: // criar lista
+                    do {
+                        System.out.println("Digite o tamanho da lista (>=2): ");
+                        tamanho = scanner.nextInt();
+                        System.out.println();
+                    }while(!lista.setMaxTam(tamanho));
+                    break;
+
+                case 2: // inserir no final
+                    contato = criarContato();
+                    if(lista.insereFinal(contato)) {
+                        System.out.println("Sucesso! O contato foi inserido no final da lista.");
+                        System.out.println();
+                    }else {
+                        System.out.println("Falha ao inserir contato! Lista cheia.");
+                        System.out.println();
+                    }
+                    break;
+
+                case 3: // pesquisar
+                    long cpf;
+                    System.out.println("Digite o CPF do contato que deseja pesquisar: ");
+                    cpf = scanner.nextLong();
+                    contato = new Contato(null, null, cpf);
+                    contato = lista.pesquisa(contato);
+                    if(contato == null) {
+                        System.out.println("Falha ao pesquisar contato! O CPF procurado n�o existe na lista");
+                        System.out.println();
+                    } else {
+                        System.out.println("Sucesso! Contato encontrado.");
+                        System.out.println();
+                        System.out.println(contato.getContato());
+                        System.out.println();
+                    }
+                    break;
+
+                case 4: // retirar
+                    System.out.println("Digite o CPF do contato que deseja retirar da lista: ");
+                    cpf = scanner.nextLong();
+                    contato = new Contato(null, null, cpf);
+                    contato = lista.retira(contato);
+                    if(contato == null) {
+                        System.out.println("Falha ao retirar contato! O CPF procurado n�o existe na lista");
+                        System.out.println();
+
+                    } else {
+                        System.out.println("Sucesso! Contato retirado.");
+                        System.out.println();
+                        System.out.println(contato.getContato());
+                        System.out.println();
+                    }
+                    break;
+
+                case 5:
+                    System.out.println(lista.getLista());
+                    break;
+
+                case 6:
+                    System.out.println("Programa encerrado.");
+                    break;
+            }
+
+        } while (opcao != 6);
+    }
+}
 
