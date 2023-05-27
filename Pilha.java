ede2397d48eef6268b22d171002cff7fcfff2887
Heloíse Cardoso <104039569+HeloiseCardoso@users.noreply.github.com>
2023-05-27 12:32:04 -0300

    Update Pilha.java

--- Pilha.java
+++ Pilha.java
@@ -59,3 +59,121 @@ public class Pilha{
 
 
 }
+
+
+public class Contato {
+    private String nome;
+    private long cpf;
+    private String endereco;
+
+
+    public Contato(String nome,  String endereco, long cpf) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.cpf = cpf;
+
+    }
+
+    public String getContato(){
+        String auxiliar = nome + "\n";
+        auxiliar = auxiliar + endereco + "\n";
+        auxiliar = auxiliar + Long.toString(cpf) + "\n";
+        return auxiliar;
+    }
+
+}
+
+
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+    static Contato criarContato() {
+        System.out.println("Nome: ");
+        ;
+        scanner.skip("\\R");
+        String nome = scanner.nextLine();
+        System.out.println("Endereco: ");
+        String endereco = scanner.nextLine();
+        System.out.println("cpf: ");
+        long cpf = scanner.nextLong();
+
+        Contato contato = new Contato(nome, endereco, cpf);
+        return contato;
+    }
+
+
+    static Contato inverterPilha(Pilha auxiliarPilha){
+        Pilha inverterPilha = new Pilha();
+        while(!auxiliarPilha.pilhaVazia()){
+            inverterPilha.push(auxiliarPilha.pop());
+            return inverterPilha;
+        }
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Pilha pilha = new Pilha();
+        Contato auxiliar = null;
+        int numero;
+
+        do {
+            System.out.println("Tamanho: ");
+            numero = scanner.nextInt();
+        } while (!pilha.setMaxTamanho(numero));
+        do {
+            System.out.println("Digite: ");
+            System.out.println("1: Para empilhar");
+            System.out.println("2: Para desempilhar");
+            System.out.println("3: Para encerrar.");
+            System.out.println("3: Inverter pilha")
+            numero = scanner.nextInt();
+
+            switch (numero) {
+                case 1:
+                    auxiliar = criarContato();
+                    if (pilha.push(auxiliar)) {
+                        System.out.println("Sucesso!");
+                    } else {
+                        System.out.println("Pilha cheia!.");
+                        auxiliar = null;
+                        break;
+                    }
+                case 2:
+                    auxiliar = pilha.pop();
+                    if (auxiliar == null) {
+                        System.out.println("Erro: pilha vazia");
+                    } else {
+                        System.out.println(auxiliar.getContato());
+                        auxiliar = null;
+                        break;
+                    }
+                case 3:
+                    pilha = inveterPilha(pilha);
+                case 4:
+                    System.out.println("programa encerrado.");
+                    break;
+                default:
+                    System.out.println("Opacao invalida! ");
+            }
+        } while (numero !=4);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
